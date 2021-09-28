336c9998a0d5aa945d1841d37b951326b97f7406
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2021-09-28 23:42:42 +0200

    Create WindowManager Constructor

---
+++ src/managers/WindowManager.py
@@ -0,0 +1,14 @@
+#WindowManager.py
+
+import cv2
+import numpy as np
+import time
+
+class WindowManager(object):
+
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+
+        self._windowName = windowName
+        self._isWindowCreated = False
+
